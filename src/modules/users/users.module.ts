f31679c09bc9d500491ae6edea93174bfc86ea0d
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-12-27 13:10:45 -0300

    feat: create database module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { PrismaService } from 'src/shared/database/prisma.service';
-
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
