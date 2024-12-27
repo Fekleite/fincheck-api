b20ab427545f16406c9b52747f78465b6403d355
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-12-27 11:44:24 -0300

    feat: verify conflict error on email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -9,11 +9,23 @@ export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const { email, name, password } = createUserDto;
+
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use.');
+    }
+
     const user = await this.prismaService.user.create({
       data: {
-        name: createUserDto.name,
-        email: createUserDto.email,
-        password: createUserDto.password,
+        name,
+        email,
+        password,
       },
     });
 
