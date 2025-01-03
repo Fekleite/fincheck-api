94904879123e03c7ed7f4b06e3e32c6880370f73
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-03 15:12:04 -0300

    feat: create decorator to define public routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,6 +5,9 @@ import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signIn.dto';
 import { SignUpDto } from './dto/signup.dto';
 
+import { IsPublic } from 'src/shared/decorators/IsPublic';
+
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
