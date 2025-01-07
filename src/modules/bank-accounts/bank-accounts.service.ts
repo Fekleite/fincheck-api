7f57fe0190fc757181ccc7a88804af6dc1433cf2
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-07 19:55:18 -0300

    feat: add create bank account route

--- src/modules/bank-accounts/bank-accounts.service.ts
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
+
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-account.repositories';
+
 @Injectable()
 export class BankAccountsService {
-  create(createBankAccountDto: CreateBankAccountDto) {
-    return 'This action adds a new bankAccount';
+  constructor(private bankAccountsRepository: BankAccountsRepository) {}
+
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    return this.bankAccountsRepository.create({
+      data: {
+        userId,
+        name: createBankAccountDto.name,
+        initialBalance: createBankAccountDto.initialBalance,
+        color: createBankAccountDto.color,
+        type: createBankAccountDto.type,
+      },
+    });
   }
 
   findAll() {
